3d1877e8e39fcef9974cd12dbbfc562fcf6ccb25
DAYANIDHARM <dayanidharm2024katargam@gmail.com>
2025-12-07 17:14:17 +0530

    live

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ var usersRouter = require('./routes/users');
 var testRouter = require('./routes/login');
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/testdb");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected successfully"))
+.catch(err => console.log("MongoDB connection error:", err));
+
+
 
 var app = express();
 
